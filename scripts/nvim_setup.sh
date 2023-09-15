7a4658f09e166314cf9aa8b7030db5a70d2533fc
TheInquisitor <28104568+Thelnquisitor@users.noreply.github.com>
2023-09-15 20:22:34 +0100

    Added nvim and tmux install to script

--- scripts/nvim_setup.sh
+++ scripts/nvim_setup.sh
@@ -1,17 +1,38 @@
-# Pull down neovim plugin manager (Packer)
-git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+#!/bin/bash
 
-# Telescope dependency
-sudo apt-get install ripgrep
-
-# Install language compilers
-sudo apt-get install clang
-sudo apt-get install golang
+# Update the system
+sudo apt update
+sudo apt upgrade
 
 # Install prerequisite tools
-sudo apt-get install unzip
-sudp apt-get install npm
+sudo apt install unzip
+sudp apt install npm
 
-# tmux (installing 3.3a manually)
-#
-sudo apt-get install libevent-dev
+# Pull nvim 0.9.2 release and install
+wget https://github.com/neovim/neovim/releases/download/v0.9.2/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+ln -s ./nvim-linux64/bin/nvim /usr/bin/nvim
+rm nvim-linux64.tar.gz
+
+# Pull down neovim plugin manager (Packer)
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Telescope dependency
+sudo apt install ripgrep
+
+# Install language compilers
+sudo apt install clang
+sudo apt install golang
+
+# Tmux dependencies
+sudo apt install libevent-dev
+sudo apt install pkg-config
+sudo apt install ncurses-dev
+
+# Pull tmux 3.3a and install
+wget https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz
+tar -xzvf tmux-3.3a.tar.gz
+cd tmux-3.3a
+./configure
+make && sudo make install
+rm tmux-3.3a.tar.gz
