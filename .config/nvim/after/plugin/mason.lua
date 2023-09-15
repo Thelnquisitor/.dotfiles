e619b00389cc505b8577da8f749769f4632e5c08
TheInquisitor <28104568+Thelnquisitor@users.noreply.github.com>
2023-09-16 00:22:53 +0100

    Added mason config for install checks

---
+++ .config/nvim/after/plugin/mason.lua
@@ -0,0 +1,26 @@
+local ensure_installed = function()
+    local packages = {
+        "jq",
+        "gofumpt",
+        "stylua",
+        "clangd",
+        "golangci-lint-langserver",
+        "lua-language-server",
+        "goimports-reviser",
+    }
+
+    local registry = require("mason-registry")
+
+    local filteredPackages = {}
+    for _, v in pairs(packages) do
+        if not registry.is_installed(v) then
+            table.insert(filteredPackages, v)
+        end
+    end
+
+    if #filteredPackages > 0 then
+        require("mason.api.command").MasonInstall(filteredPackages)
+    end
+end
+
+ensure_installed()
